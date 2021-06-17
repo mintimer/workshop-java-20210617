de68f43874113a1495f29162d20a28fe090548df
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-17 11:18:21 +0700

    Employee Add

---
+++ Demo01/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Employee somkiat = new Employee(); //Default Constructor
+        String result = somkiat.getFullName();
+        System.out.println(result);
+    }
+}
