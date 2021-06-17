31c9db223449ec70a58a7cc875145753dc5c9c16
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-17 11:43:21 +0700

    Get Args to Name

--- Demo01/src/Main.java
+++ Demo01/src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Employee somkiat = new Employee(); //Default Constructor
+        String fisrtname = args[0];
+        String lastname = args[1];
+        Employee somkiat = new Employee(fisrtname,lastname); //Default Constructor
+
         String result = somkiat.getFullName();
         System.out.println(result);
     }
