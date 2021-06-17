f37df2faf2a7eab63233e5a0ce00ee0be7fdefbe
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-17 11:15:54 +0700

    Employee Add

--- Demo01/src/Employee.java
+++ Demo01/src/Employee.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Employee {
+public class Employee {
+    // Properties ot States
+    private int id;
+    private String firstname;
+    private String lastname;
+   //Department department;
+    private String positionName;
+
+    // Behaviors or Methods
+    void doSmt(){
+
+    }
+
+    public String getFullName() {
+        return "FAKE RESULT";
+    }
+
+    String getPositionName() {
+        return "";
+    }
 }
