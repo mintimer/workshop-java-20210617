607cc1c9572c6464b9d54e69e1c6a42589700af3
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-17 11:59:50 +0700

    Add Department

--- Demo01/src/Employee.java
+++ Demo01/src/Employee.java
@@ -3,7 +3,7 @@ public class Employee {
     private int id;
     private String firstname;
     private String lastname;
-    //Department department;
+    private Department department;
     private String positionName;
 
     //Constructor
