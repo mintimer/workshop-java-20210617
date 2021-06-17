bf6dbeb0c0b50d2054207553e6567d813da97e7a
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-17 16:03:14 +0700

    start number

--- Demo01/src/MyRange2.java
+++ Demo01/src/MyRange2.java
@@ -12,4 +12,14 @@ public class MyRange2 {
     public boolean StartWithExclude() {
         return input.charAt(0) == '(';
     }
+
+    public int getStartNumber() {
+        int indexOfComma = input.indexOf(",");
+        String stringNum = input.substring(1,indexOfComma);
+        int startNumber = Integer.parseInt(stringNum);
+        if(StartWithInclude()){
+            return startNumber;
+        }
+        return startNumber + 1;
+    }
 }
--- Demo01/src/MyRange2Test.java
+++ Demo01/src/MyRange2Test.java
@@ -17,4 +17,18 @@ class MyRange2Test {
         boolean isExclude = myRange2.StartWithExclude();
         assertTrue(isExclude);
     }
+
+    @Test
+    public void StartNumberWithInclude() {
+        MyRange2 myRange2 = new MyRange2("[1,5]");
+        int startNumber = myRange2.getStartNumber();
+        assertEquals(1,startNumber);
+    }
+
+    @Test
+    public void StartNumberWithExclude() {
+        MyRange2 myRange2 = new MyRange2("(10,5]");
+        int startNumber = myRange2.getStartNumber();
+        assertEquals(11,startNumber);
+    }
 }
