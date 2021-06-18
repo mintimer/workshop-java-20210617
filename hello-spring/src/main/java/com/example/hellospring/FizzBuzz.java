18080095ff9673c058d0d97a88e9ef0e004f9266
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-18 14:56:50 +0700

    case 1

--- hello-spring/src/main/java/com/example/hellospring/FizzBuzz.java
+++ hello-spring/src/main/java/com/example/hellospring/FizzBuzz.java
@@ -1,4 +1,7 @@
 package com.example.hellospring;
 
 public class FizzBuzz {
+    public String get(int i) {
+        return i + "";
+    }
 }
--- hello-spring/src/test/java/com/example/hellospring/FizzBuzzTest.java
+++ hello-spring/src/test/java/com/example/hellospring/FizzBuzzTest.java
@@ -1,7 +1,18 @@
 package com.example.hellospring;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+
+    @Test
+    @DisplayName("1=>1")
+    public void case01 (){
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String result = fizzBuzz.get(1);
+        assertEquals("1",result);
+    }
 
 }
