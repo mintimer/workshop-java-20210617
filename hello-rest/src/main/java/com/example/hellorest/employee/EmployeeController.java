c05567f134ba178c410e3e460cefe7dbf5ee98ca
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-21 11:41:52 +0700

    get path variable

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -1,13 +1,14 @@
 package com.example.hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
     @GetMapping("/employee/{id}")
-    public EmployeeResponse getEmployeeById(int id) {
+    public EmployeeResponse getEmployeeById(@PathVariable int id) {
         return new EmployeeResponse(id, "Somkiat", "Pui");
     }
 
