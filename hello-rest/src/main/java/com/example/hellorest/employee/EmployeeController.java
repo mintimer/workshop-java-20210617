952a4ea0232ce806f1cdcd6043d3a631f18caae4
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-21 13:40:11 +0700

    working with req param

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.hellorest.employee;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +10,21 @@ public class EmployeeController {
 
     @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeById(@PathVariable String id) {
+        // Validate ID Number Only
+        int _id = 0;
+        try {
+            _id = Integer.parseInt(id);
+        }
+        catch (NumberFormatException ignored){
+
+        }
+        return new EmployeeResponse(_id, "Somkiat", "Pui");
+    }
+
+    // employee?id=?
+
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeById2(@RequestParam String id) {
         // Validate ID Number Only
         int _id = 0;
         try {
