289ad96118efac9dcc21dc5a105d60566faa43aa
Settapong Supkong <settapong.s@kbtg.tech>
2021-06-21 11:38:05 +0700

    add response

--- hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
+++ hello-rest/src/main/java/com/example/hellorest/employee/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.example.hellorest.employee;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
-    public EmployeeResponse getEmployeeById(Int id) {
+    @GetMapping("/employee/{id}")
+    public EmployeeResponse getEmployeeById(int id) {
         return new EmployeeResponse(id, "Somkiat", "Pui");
     }
 
